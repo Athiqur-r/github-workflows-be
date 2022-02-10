1273631af7c6028c35d0ec0b3aecef115aeecdee
Athiqur-r <athiqur-r@innomick.com>
2022-02-10 20:10:01 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello World!'+'\n'+'Navigate to /raheman to get fullName');
+});
+
+app.get('/raheman', function (req, res) {
+  res.status(200).send('I am Mohammad Athiqur Raheman');
+});
+
+var port = process.env.PORT || 8000;
+
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
